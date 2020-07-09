bba0c7676cd612bb500e29f2d342b2694153d147
caioluis <ocaioluis@gmail.com>
2020-07-09 23:41:14 +0100

    Sistema de defeitos

--- qualidadesDefeitos.js
+++ qualidadesDefeitos.js
@@ -1,6 +1,8 @@
-var pontosDistribuidos = 0;
+var pontosQualidades = 0;
+var pontosDefeitos = 0;
 
 var maxPontosQualidades = getQualidadesMaxPoints();
+var minPontosDefeitos = getDefeitosMinPoints();
 
 document.getElementById("qualidades-contador").innerHTML = "Qualidades - Pontos restantes: " + (maxPontosQualidades);
 
@@ -15,19 +17,38 @@ function adicionarQualidade(element) {
             return false;
         } else{
             element.classList.add("escolhido");
-            pontosDistribuidos += valorQualidade;
+            pontosQualidades += valorQualidade;
         }
     } else {
         element.classList.remove("escolhido");
-        pontosDistribuidos -= valorQualidade;
+        pontosQualidades -= valorQualidade;
     }
 
     document.getElementById("qualidades-contador").innerHTML = "Qualidades - Pontos restantes: " + getQualidadesMaxPoints();
+    document.getElementById("defeitos-contador").innerHTML = "Defeitos - Pontos necessários: " + getDefeitosMinPoints();
 
+}
+
+function adicionarDefeito(element) {
+    this.minPontosDefeitos = getDefeitosMinPoints();
+    var valorDefeito = parseInt(element.value);
+    
+    if(element.className == ""){
+        element.classList.add("escolhido");
+        pontosDefeitos += valorDefeito;
+    } else {
+        element.classList.remove("escolhido");
+        pontosDefeitos -= valorDefeito;
+    }
+
+    document.getElementById("defeitos-contador").innerHTML = "Defeitos - Pontos necessários: " + getDefeitosMinPoints();
+    
 }
 
 function semClaHandler2(field){
     document.getElementById("qualidades-contador").innerHTML = "Qualidades - Pontos restantes: " + getQualidadesMaxPoints();
+    document.getElementById("defeitos-contador").innerHTML = "Defeitos - Pontos necessários: " + getDefeitosMinPoints();
+
     
     if(field.checked){
         document.getElementById('kg').disabled = true;
@@ -40,7 +61,14 @@ function semClaHandler2(field){
 
 function getQualidadesMaxPoints()
 {
-    
-    return (4 + Number(document.getElementById('semCla').checked) - pontosDistribuidos);
+    return (4 + Number(document.getElementById('semCla').checked) - pontosQualidades);   
+}
+
+function getDefeitosMinPoints() {
+    if (pontosQualidades > 4) {
+        return 4 - pontosDefeitos;
+    } else{
+        return pontosQualidades - pontosDefeitos;
+    }
     
 }
