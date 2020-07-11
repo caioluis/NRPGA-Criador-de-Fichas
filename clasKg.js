1a533f257e7f76903907e2b531040e5dd07c1133
caioluis <ocaioluis@gmail.com>
2020-07-11 23:27:33 +0100

    Correção

--- clasKg.js
+++ clasKg.js
@@ -6,11 +6,13 @@ toggleText.addEventListener("click", function claKgToggle () {
     
     if (toggleText.innerHTML == "Clã") {
         toggleText.innerHTML = "Kekkei Genkai";
+        document.getElementById("cla-dropdown").value = "--"
         document.getElementById("cla-dropdown").style.display = "none";
         document.getElementById("kg-dropdown").style.display = "inline-block";
         
     } else {
         toggleText.innerHTML = "Clã";
+        document.getElementById("kg-dropdown").value = "--"
         document.getElementById("kg-dropdown").style.display = "none";
         document.getElementById("cla-dropdown").style.display = "inline-block";
     }
