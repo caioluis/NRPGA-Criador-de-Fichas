a3f458922a26be3e2618a228e367ec1871e0c883
caioluis <ocaioluis@gmail.com>
2020-07-11 23:18:40 +0100

    Gerenciar clãs/kgs e suas habilidades

---
+++ clasKg.js
@@ -0,0 +1,17 @@
+
+var toggleText = document.getElementById("clakg-label");
+
+toggleText.addEventListener("click", function claKgToggle () {
+    console.log("opa")
+    
+    if (toggleText.innerHTML == "Clã") {
+        toggleText.innerHTML = "Kekkei Genkai";
+        document.getElementById("cla-dropdown").style.display = "none";
+        document.getElementById("kg-dropdown").style.display = "inline-block";
+        
+    } else {
+        toggleText.innerHTML = "Clã";
+        document.getElementById("kg-dropdown").style.display = "none";
+        document.getElementById("cla-dropdown").style.display = "inline-block";
+    }
+    });
