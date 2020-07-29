44f35aeaec315146771c19768579715f00cfcbc9
caioluis <ocaioluis@gmail.com>
2020-07-30 00:05:24 +0100

    Adicionar os nerfs dos defeitos

--- scripts/qualidadesDefeitos.js
+++ scripts/qualidadesDefeitos.js
@@ -21,28 +21,34 @@ function adicionarQualidade(botão) {
 }
 
 function removerQualidade(botão) {
-    botão.removeClass('qualidade-escolhida');
-    valorQualidade = parseInt(botão.val());
-    pontosQualidades -= valorQualidade;
-    removerBonusDatabook(botão);
+    if(botão.hasClass('qualidade-escolhida')){
+        botão.removeClass('qualidade-escolhida');
+        valorQualidade = parseInt(botão.val());
+        pontosQualidades -= valorQualidade;
+        removerBonusDatabook(botão);
+    }
 }
+    
 
 function adicionarDefeito(botão) {
     botão.addClass('defeito-escolhido');
     valorDefeito = parseInt(botão.val());
     pontosDefeitos += valorDefeito;
-    adicionarNerfDatabook();
+    adicionarNerfDatabook(botão);
 }
 
 function removerDefeito(botão) {
-    botão.removeClass('defeito-escolhido');
-    valorDefeito = parseInt(botão.val());
-    pontosDefeitos -= valorDefeito;
+    if(botão.hasClass('defeito-escolhido')){
+        botão.removeClass('defeito-escolhido');
+        valorDefeito = parseInt(botão.val());
+        pontosDefeitos -= valorDefeito;
+        removerNerfDatabook(botão);
+    }
 }
 
 
 $('#qualidades .qualidades-defeitos :button').click(function(){
-    botão = $(this);
+    var botão = $(this);
     maxPontosQualidades = getQualidadesMaxPoints();
     valorQualidade = parseInt(botão.val());
     if( (botão.attr('class') == undefined) || (botão.attr('class') == '') ){
@@ -71,10 +77,9 @@ $('#qualidades .qualidades-defeitos :button').click(function(){
 });
 
 $('#defeitos .qualidades-defeitos :button').click(function(){
-    botão = $(this);
+    var botão = $(this);
     minPontosDefeitos = getDefeitosMinPoints();
     valorDefeito = parseInt(botão.val());
-    botão = $(this)
     if((botão.attr('class') == undefined) || (botão.attr('class') == '')){
        adicionarDefeito(botão)
     } else {
@@ -108,6 +113,7 @@ function adicionarBonusDatabook(botão) {
             $('#ninjutsu-label span').text('+1');
             bonusNin++;
             quantBonus++;
+            limiteNin += 2;
         }
 
         if (botão.text().includes('Habilidade em Taijutsu')) {
@@ -120,7 +126,7 @@ function adicionarBonusDatabook(botão) {
         if (botão.text().includes('Habilidade em Genjutsu')) {
             $('#genjutsu-label span').show();
             $('#genjutsu-label span').text('+1');
-            bonusTai++;
+            bonusGen++;
             quantBonus++;
         }
 
@@ -152,6 +158,18 @@ function adicionarBonusDatabook(botão) {
             quantBonus++;
         }
     }
+
+    if (botão.text().includes('Habilidade em Ninjutsu')) {
+        $('#defeitoNin').hide();
+    }
+
+    if (botão.text().includes('Habilidade em Genjutsu')) {
+        $('#defeitoGen').hide();
+    }
+
+    if (botão.text().includes('Prodígio')) {
+        $('#defeitoDificuldade').hide();
+    } 
 }
 
 function removerBonusDatabook(botão) {
@@ -160,6 +178,7 @@ function removerBonusDatabook(botão) {
             $('#ninjutsu-label span').hide();
             bonusNin--;
             quantBonus--;
+            limiteNin -= 2;
         }
     }
 
@@ -174,49 +193,271 @@ function removerBonusDatabook(botão) {
     if (botão.text().includes('Habilidade em Genjutsu')) {
         if ($('#genjutsu-label span').text() == '+1') {
             $('#genjutsu-label span').hide();
-            bonusTai--;
+            bonusGen--;
             quantBonus--;
         }
     }
 
     if (botão.text().includes('Força Aguçada')) {
-        if ($('#forca-label span').text() == '+1') {
+        if ($('#forca-label span').text() == '+1' || $('#forca-label span').text() == '0') {
             $('#forca-label span').hide();
             bonusForca--;
-            quantBonus--;   
+            quantBonus--;
         }
     }
 
     if (botão.text().includes('Inteligência Aguçada')) {
         if ($('#int-label span').text() == '+1') {
-            $('#int-label span').show();
-            $('#int-label span').text('+1')
+            $('#int-label span').hide();
             bonusInt--;
             quantBonus--;
         }   
     }
 
     if (botão.text().includes('Agilidade Aguçada')) {
-        if ($('#vel-label span').text() = '+1') {
-            $('#vel-label span').show();
-            bonusVel++;
-            quantBonus++;
+        if ($('#vel-label span').text() == '+1') {
+            $('#vel-label span').hide();
+            bonusVel--;
+            quantBonus--;
         }
     }
 
     if (botão.text().includes('Grande Durabilidade')) {
-        if ($('#sta-label span').text() = '+1') {
-            $('#sta-label span').show();
+        if ($('#sta-label span').text() == '+1') {
+            $('#sta-label span').hide();
             bonusSta--;
             quantBonus--; 
         }
     }
+
+    if (botão.text().includes('Habilidade em Ninjutsu')) {
+        $('#defeitoNin').show();
+    }
+
+    if (botão.text().includes('Habilidade em Genjutsu')) {
+        $('#defeitoGen').show();
+    }
+
+
+    if (botão.text().includes('Prodígio')) {
+        $('#defeitoDificuldade').show();
+    } 
 }
 
 function adicionarNerfDatabook(botão) {
+    if (botão.text().includes('Fraqueza Muscular')) {
+        $('#forca-label span').show();
+        $('#forca-label span').text('0');
+        $('#forca').val(0);
+        $('#forca').prop('disabled', true);
+        removerQualidade($('#qualidadeForca'));
+        $('#qualidadeForca').hide();
+    }
+
+    if (botão.text().includes('Baixa Coordenação Motora')) {
+        removerQualidade(qualidadeTaijutsu);
+        qualidadeTaijutsu.hide();
+    }
+
+    if (botão.text().includes('Reflexos Retardados')) {
+        removerQualidade($('#qualidadeVel'));
+        $('#qualidadeVel').hide();
+    }
+
+    if (botão.text().includes('Visão Prejudicada')) {
+        removerQualidade($('#qualidadeVisao'));
+        $('#qualidadeVisao').hide();
+    }
+
+    if (botão.text().includes('Audição Prejudicada')) {
+        removerQualidade($('#qualidadeAudicao'));
+        $('#qualidadeAudicao').hide();
+    }
+
+    if (botão.text().includes('Audição Prejudicada')) {
+        removerQualidade($('#qualidadeAudicao'));
+        $('#qualidadeAudicao').hide();
+    }
+
     if (botão.text().includes('Gordo')) {
         $('#vel-label span').show();
         $('#vel-label span').text('-1');
+        $('#defeitoMagricelo').hide();
         nerfVel++;
+    }
+
+    if (botão.text().includes('Magricelo')) {
+        $('#sta-label span').show();
+        $('#sta-label span').text('-1');
+        $('#defeitoGordo').hide();
+        nerfSta++;
+    }
+
+    if (botão.text().includes('Nanismo')) {
+        $('#forca-label span').show();
+        $('#forca-label span').text('0');
+        $('#forca').val(0);
+        $('#forca').prop('disabled', true);
+        removerQualidade($('#qualidadeForca'));
+        $('#qualidadeForca').hide();
+        $('#defeitoGigantismo').hide();
+    }
+
+    if (botão.text().includes('Gigantismo')) {
+        $('#defeitoNanismo').hide();
+    }
+
+    if (botão.text().includes('Amnésia Dissociativa')) {
+        removerQualidade($('#qualidadeMemoria'));
+        $('#qualidadeMemoria').hide();
+    }
+
+    if (botão.text().includes('Impulsivos')) {
+        $('#int-label span').show();
+        $('#int-label span').text('-1');
+        nerfInt++;
+    }
+
+    if (botão.text().includes('Imunidade Baixa')) {
+        $('#vel-label span').show();
+        if ($('#vel-label span').text() == '-1') {
+            $('#vel-label span').text('-2');
+        } else {
+            $('#vel-label span').text('-1');
+        }
+
+        $('#forca-label span').show();
+        if ($('#forca-label span').text() == '-1') {
+            $('#forca-label span').text('-2');
+        } else {
+            $('#forca-label span').text('-1');
+        }
+        nerfVel++;
+        nerfForca++;
+    }
+
+    if (botão.text().includes('Inabilidade em Ninjutsu')) {
+        $('#ninjutsu-label span').show();
+        $('#ninjutsu-label span').text('0');
+        $('#ninjutsu').val(0);
+        $('#ninjutsu').prop('disabled', true);
+        removerQualidade($(qualidadeNinjutsu));
+        $(qualidadeNinjutsu).hide();
+    }
+
+    if (botão.text().includes('Inabilidade em Genjutsu')) {
+        $('#genjutsu-label span').show();
+        $('#genjutsu-label span').text('0');
+        $('#genjutsu').val(0);
+        $('#genjutsu').prop('disabled', true);
+        removerQualidade($(qualidadeGenjutsu));
+        $(qualidadeGenjutsu).hide();
+    }
+
+    if (botão.text().includes('Dificuldade em Aprender')) {
+        removerQualidade($('#qualidadeProdigio'));
+        $('#qualidadeProdigio').hide()
+    }
+}
+
+function removerNerfDatabook(botão) {
+    if (botão.text().includes('Fraqueza Muscular')) {
+        if($('#forca-label span').text() == '0') {
+            $('#forca-label span').hide();
+            $('#forca').prop('disabled', false);
+            $('#qualidadeForca').show();
+        }
+    }
+
+    if (botão.text().includes('Baixa Coordenação Motora')) {
+            qualidadeTaijutsu.show();
+    }
+
+    if (botão.text().includes('Reflexos Retardados')) {    
+            $('#qualidadeVel').show();
+    }
+
+    if (botão.text().includes('Visão Prejudicada')) {
+        $('#qualidadeVisao').show();
+    }
+
+    if (botão.text().includes('Audição Prejudicada')) {
+        $('#qualidadeAudicao').show();
+    }
+
+    if (botão.text().includes('Gordo')) {
+        if($('#vel-label span').text() == '-1') {
+            $('#vel-label span').hide();
+            $('#defeitoMagricelo').show();
+            nerfVel--;
+        }
+    }
+
+    if (botão.text().includes('Magricelo')) {
+        if($('#sta-label span').text() == '-1') {
+            $('#sta-label span').hide();
+            $('#defeitoGordo').show();
+            nerfSta--;
+        }
+    }
+
+    if (botão.text().includes('Nanismo')) {
+        if($('#forca-label span').text() == '0') {
+            $('#forca-label span').hide();
+            $('#forca').prop('disabled', false);
+            $('#qualidadeForca').show();
+            $('#defeitoGigantismo').show();
+        }
+    }
+
+    if (botão.text().includes('Gigantismo')) {
+        $('#defeitoNanismo').show();
+    }
+
+    if (botão.text().includes('Amnésia Dissociativa')) {
+        $('#qualidadeMemoria').show();
+    }
+
+    if (botão.text().includes('Impulsivos')) {
+        if($('#int-label span').text() == '-1') {
+            $('#int-label span').hide();
+            nerfVel--;
+        }
+    }
+
+    if (botão.text().includes('Imunidade Baixa')) {
+        if ($('#vel-label span').text() == '-2') {
+            $('#vel-label span').text('-1');
+        } else {
+            $('#vel-label span').hide();
+        }
+
+        if ($('#forca-label span').text() == '-2') {
+            $('#forca-label span').text('-1');
+        } else {
+            $('#forca-label span').hide();
+        }
+        nerfVel--;
+        nerfForca--;
+    }
+
+    if (botão.text().includes('Inabilidade em Ninjutsu')) {
+        if ($('#ninjutsu-label span').text() == '0') {
+            $('#ninjutsu-label span').hide();
+            $('#ninjutsu').prop('disabled', false);
+            $(qualidadeNinjutsu).show();
+        }
+    }
+
+    if (botão.text().includes('Inabilidade em Genjutsu')) {
+        if ($('#genjutsu-label span').text() == '0') {
+            $('#genjutsu-label span').show();
+            $('#genjutsu').prop('disabled', false);
+            $(qualidadeGenjutsu).show();
+        }
+    }
+
+    if (botão.text().includes('Dificuldade em Aprender')) {
+        $('#qualidadeProdigio').show()
     }
 }
