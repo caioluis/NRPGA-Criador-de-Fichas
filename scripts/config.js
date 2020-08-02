0f1dcec43a1c3b30db4137e3efc3a1cf82d0f736
caioluis <ocaioluis@gmail.com>
2020-08-02 19:26:44 +0100

    Configurar o CKEditor

--- scripts/config.js
+++ scripts/config.js
@@ -1,37 +1,23 @@
+CKEDITOR.addCss( '.cke_editable b { font-size: 20px; line-height: 15px; display: block; font-weight: 900; text-transform: uppercase; border-bottom: 1px solid #e0e0e0; color: #b5b5b5; padding-left: 5px; padding-bottom: 10px; margin-bottom: 10px;}');
 editor = CKEDITOR.replace('historia', {
-height: 300,
+autoParagraph: false,
+height: 420,
 language: 'pt-br',
-coreStyles_bold: { element: 'strong', overrides: 'false'},
+coreStyles_bold: { element: 'b', overrides: 'strong'},
 toolbarGroups: [
 	{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-	{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-	{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-	{ name: 'forms', groups: [ 'forms' ] },
-	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-	{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-	{ name: 'links', groups: [ 'links' ] },
-	{ name: 'insert', groups: [ 'insert' ] },
-	{ name: 'styles', groups: [ 'styles' ] },
-	{ name: 'colors', groups: [ 'colors' ] },
-	{ name: 'tools', groups: [ 'tools' ] },
-	{ name: 'others', groups: [ 'others' ] },
-	{ name: 'about', groups: [ 'about' ] }
-],
-
-removeButtons: 'Save,Templates,NewPage,Preview,Print,Cut,Undo,Copy,Redo,Paste,PasteText,PasteFromWord,Find,Replace,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Subscript,Superscript,Outdent,Indent,Blockquote,CreateDiv,BidiLtr,BidiRtl,Language,Link,Unlink,Anchor,Image,Flash,Table,HorizontalRule,PageBreak,Iframe,Styles,Format,Font,FontSize,ShowBlocks,About'
-})
-
-editor.addCommand("inserirSubtitulo", {
-	exec : function( editor ) {
-		var textoSelecionado = editor.getSelection().getSelectedText();
-		var novoSubtitulo = new CKEDITOR.dom.element("b");
-		novoSubtitulo.setText(textoSelecionado);
-		editor.insertElement(novoSubtitulo);
-	  }
-});
-editor.ui.addButton('Subtitulo', {
-    label: "Adicionar subtítulo",
-    command: 'inserirSubtitulo',
-    toolbar: 'insert',
-    icon: 'https://image.flaticon.com/icons/svg/52/52326.svg'
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	],
+	removeButtons: 'Source,Save,NewPage,Preview,Print,Templates,Cut,Copy,Paste,PasteFromWord,Undo,Redo,Find,Replace,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Subscript,Superscript,CopyFormatting,RemoveFormat,NumberedList,BulletedList,Outdent,Indent,Blockquote,CreateDiv,BidiLtr,BidiRtl,Language,Link,Unlink,Anchor,Image,Flash,Table,HorizontalRule,Smiley,PageBreak,Iframe,Styles,Format,Font,FontSize,ShowBlocks,About,PasteText'
 });
