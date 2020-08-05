f8a48428da3d27f57b4bca1782e1656878165c20
caioluis <ocaioluis@gmail.com>
2020-08-05 20:49:37 +0100

    Ajustar o script para comportar as mudanças nas qualidades

--- scripts/limiteJutsus.js
+++ scripts/limiteJutsus.js
@@ -3,6 +3,7 @@ var pontosNin = pontosTai = pontosGen = 0;
 var pericia = '';
 var contadorJutsusGerais = 0;
 var jutsusGerais = '';
+var menteImplacavel = '';
 
 $('#segunda-natureza').parent().hide();
 $('#terceira-natureza').parent().hide();
@@ -17,6 +18,9 @@ function atualizarLimites() {
     pontosNin = parseInt($('#ninjutsu').val()) + bonusNin;
     pontosTai = parseInt($('#taijutsu').val()) + bonusTai;
     pontosGen = parseInt($('#genjutsu').val()) + bonusGen;
+    pontosSelos = parseInt($('#selos').val());
+    pontosInt = parseInt($('#int').val()) + bonusInt;
+
     limiteNin = ` +${pontosNin + (bonusNin*2)} Ninjutsus`
     limiteTai = ` +${pontosTai + (bonusTai*2)} Taijutsus`
     limiteGen = ` +${pontosGen + (bonusGen*2)} Genjutsus`
@@ -45,14 +49,31 @@ function atualizarLimites() {
         jutsusGerais = ` +${contadorJutsusGerais} Jutsus Gerais`;
     }
 
-    $('#limite-jutsus').val('5 Jutsus & +1 Rank S' + jutsusGerais + limiteNin + limiteTai + limiteGen);
+    $('#limite-jutsus').val('5 Jutsus & +1 Rank S' + jutsusGerais + limiteNin + limiteTai + limiteGen + menteImplacavel);
 
     if (pontosNin > 2) {
         $('#limite-jutsus').val($('#limite-jutsus').val() + pericia)
+        ('#qualidadeElemental').show();
+    } else {
+        $('#qualidadeElemental').hide();
     }
 
     if (pontosNin == 0) {
         $('#primeira-natureza').val('--');
+    } else {
+        $('#primeira-natureza').val($('#afinidade-elemental').val());
+    }
+
+    if (pontosNin > 2 && pontosSelos > 0 && pontosInt > 2 && $('#qualidadeGCC').hasClass('qualidade-escolhida')){
+        $('#qualidadeFuin').show();
+    } else {
+        $('#qualidadeFuin').hide();
+    }
+
+    if (pontosTai > 2) {
+        $('#qualidadeArmamentista').show();
+    } else {
+        $('#qualidadeArmamentista').hide();
     }
 };
 
@@ -71,4 +92,4 @@ $('#afinidade-elemental').on('keypress', function (e) {
      if (key == 32) {
       e.preventDefault();
      }
-})
+});
